5b36677e9c6e0fef86ba2d6c5d96260e7420f8e4
Chris Fung <aergonaut@gmail.com>
2015-12-23 21:48:13 -0800

    add support fo the synchronize pull request event

--- app/mediators/receive_pull_request_event.rb
+++ app/mediators/receive_pull_request_event.rb
@@ -2,13 +2,24 @@ class ReceivePullRequestEvent
   include Sidekiq::Worker
 
   def perform(payload)
-    reviewers = payload["pull_request"]["body"].scan(/- \[.\] @(\w+)/).flatten.map(&:strip)
+    @payload = payload
+
+    case @payload["action"]
+    when "opened"
+      self.on_opened
+    when "synchronize"
+      self.on_synchronize
+    end
+  end
+
+  def on_opened
+    reviewers = @payload["pull_request"]["body"].scan(/- \[.\] @(\w+)/).flatten.map(&:strip)
     
-    number = payload["number"]
+    number = @payload["number"]
 
     PullRequest.create!(number: number, status: "pending_review", pending_reviews: reviewers)
 
-    pr_sha = payload["pull_request"]["head"]["sha"]
+    pr_sha = @payload["pull_request"]["head"]["sha"]
 
     github = Octokit::Client.new(access_token: ENV["CODY_GITHUB_ACCESS_TOKEN"])
     github.create_status(
@@ -19,5 +30,38 @@ class ReceivePullRequestEvent
       description: "Not all reviewers have approved. Comment \"LGTM\" to give approval.",
       target_url: ENV["CODY_GITHUB_STATUS_TARGET_URL"]
     )
+  end
+
+  # The "synchronize" event occurs whenever a new commit is pushed to the branch
+  # or the branch is rebased.
+  #
+  # In this case, we preserve the current review status and update the new
+  # commit with the correct status indicator.
+  def on_synchronize
+    number = @payload["number"]
+
+    if PullRequest.exists?(number: number)
+      pr = PullRequest.find_by(number: number)
+
+      status = "pending"
+      description = "Not all reviewers have approved. Comment \"LGTM\" to give approval."
+
+      if pr.status == "approved"
+        status = "success"
+        description = "Code review complete"
+      end
+
+      pr_sha = @payload["pull_request"]["head"]["sha"]
+
+      github = Octokit::Client.new(access_token: ENV["CODY_GITHUB_ACCESS_TOKEN"])
+      github.create_status(
+        ENV["CODY_GITHUB_REPO"],
+        pr_sha,
+        status,
+        context: "code-review/cody",
+        description: description,
+        target_url: ENV["CODY_GITHUB_STATUS_TARGET_URL"]
+      )
+    end
   end
 end
--- spec/mediators/receive_pull_request_event_spec.rb
+++ spec/mediators/receive_pull_request_event_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ReceivePullRequestEvent do
-  let(:payload) { JSON.load(File.open(Rails.root.join("spec", "fixtures", "pull_request.json"))) }
+  let(:payload) do
+    from_fixture = JSON.load(File.open(Rails.root.join("spec", "fixtures", "pull_request.json")))
+    from_fixture["action"] = action
+    from_fixture
+  end
 
   let(:job) { ReceivePullRequestEvent.new }
 
@@ -10,13 +14,45 @@ RSpec.describe ReceivePullRequestEvent do
       stub_request(:post, %r(https?://api.github.com/repos/\w+/\w+/statuses/[0-9abcdef]{40}))
     end
 
-    it "creates a new PullRequest" do
-      expect { job.perform(payload) }.to change { PullRequest.count }.by(1)
+    context "when the action is \"opened\"" do
+      let(:action) { "opened" }
+
+      it "creates a new PullRequest" do
+        expect { job.perform(payload) }.to change { PullRequest.count }.by(1)
+      end
+
+      it "sends a POST request to GitHub" do
+        job.perform(payload)
+        expect(WebMock).to have_requested(:post, %r(https?://api.github.com/repos/\w+/\w+/statuses/[0-9abcdef]{40}))
+      end
     end
 
-    it "sends a POST request to GitHub" do
-      job.perform(payload)
-      expect(WebMock).to have_requested(:post, %r(https?://api.github.com/repos/\w+/\w+/statuses/[0-9abcdef]{40}))
+    context "when the action is \"synchronize\"" do
+      let(:action) { "synchronize" }
+
+      let!(:pr) { FactoryGirl.create :pull_request, number: payload["number"], status: status }
+
+      before do
+        job.perform(payload)
+      end
+
+      context "and the PR is pending" do
+        let(:status) { "pending_review" }
+
+        it "sends the pending review comment in the body" do
+          expect(WebMock).to have_requested(:post, %r(https?://api.github.com/repos/\w+/\w+/statuses/[0-9abcdef]{40})).
+            with { |req| JSON.load(req.body)["description"] == "Not all reviewers have approved. Comment \"LGTM\" to give approval." }
+        end
+      end
+
+      context "and the PR is approved" do
+        let(:status) { "approved" }
+
+        it "sends the review complete comment in the body" do
+          expect(WebMock).to have_requested(:post, %r(https?://api.github.com/repos/\w+/\w+/statuses/[0-9abcdef]{40})).
+            with { |req| JSON.load(req.body)["description"] == "Code review complete" }
+        end
+      end
     end
   end
 end
