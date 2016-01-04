070ebf295b6fb4b2e3e4016948902e0e1dfc11cf
Chris Fung <aergonaut@gmail.com>
2016-01-04 11:22:50 -0800

    guard the pull request action with a senders whitelist

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,6 +2,12 @@ class WebhooksController < ApplicationController
   def pull_request
     body = JSON.load(request.body)
 
+    senders_whitelist = Setting.lookup("senders_whitelist")
+    if senders_whitelist.present? && !senders_whitelist.include?(body["sender"]["login"])
+      head :ok
+      return
+    end
+
     if body["action"] == "opened" || body["action"] == "synchronize"
       ReceivePullRequestEvent.perform_async(body.slice("action", "number", "pull_request", "repository"))
     end
