c0172ff661a26d6f6589d3d931220cfc8f90a321
Chris Fung <aergonaut@gmail.com>
2016-12-14 11:01:06 -0800

    restore memcache production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  if ENV["MEMCACHEDCLOUD_SERVERS"]		
+   config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"], :compress => true, :expires_in => 1.month }		
+  end
 end
